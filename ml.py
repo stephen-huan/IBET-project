cc6af1d80314200e6ec33a768685b02b2780ccb9
Stephen Huan <stephen03.huan@gmail.com>
2018-04-24 20:18:47 -0400

    Added Inputer, linear regression as control

--- ml.py
+++ ml.py
@@ -1,12 +1,14 @@
 import time
 import numpy as np
 import scipy
+from scipy import stats
 from sklearn import svm
 from sklearn.neural_network import MLPRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import RandomizedSearchCV, train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, Imputer
 from sklearn.metrics import mean_absolute_error, median_absolute_error, explained_variance_score, r2_score
 import matplotlib.pyplot as plt
 
@@ -19,12 +21,17 @@ def getDataUnlabeled():
 
 def getDataLabeled():
     features, classes = [], []
-    for line in open("2016HistoricalData.csv").read().split("\n"):
+    for line in open("Final.csv").read().split("\n")[1:]:
         line = line.split(",")
-        if len(line) == 5:
-            features.append(line[1:])
-            classes.append(line[0])
-    return np.array(features[1:], dtype=float), np.array(classes[1:], dtype=float)
+        if len(line) > 1:
+            cell = [x if x != "" else np.nan for x in line[:-1]]
+            cell.insert(0, ord(cell[0][0]))
+            cell[1] = cell[1][1:]
+            features.append(cell)
+            eggs = line[-1] if line[-1] != "" else np.nan
+            classes.append(eggs)
+    mean = sum([int(x) if x == x else 0 for x in classes])/len(classes)
+    return Imputer(missing_values='NaN', strategy='mean', axis=0).fit(features).transform(features), np.array([x if x == x else mean for x in classes], dtype=float)
 
 def stratify(data_X, data_y, feature):
     out_X = {}
@@ -40,7 +47,7 @@ def stratify(data_X, data_y, feature):
     return out_X, out_y
 
 def getFinalData():
-    data = stratify(*getDataLabeled(), 0)
+    data = stratify(*getDataLabeled(), 2)
     i = iter(data[0])
     temp = next(i)
     final = train_test_split(data[0][temp], data[1][temp], test_size=0.2)
@@ -65,21 +72,21 @@ def test(y_test, y_pred, human):
 def train(name, model, param, X_train, X_test, y_train, y_test, human=True):
     print(name + ":")
     start_time = time.time()
-    regr = RandomizedSearchCV(model, param, n_iter=100).fit(X_train, y_train)
+    regr = RandomizedSearchCV(model, param, n_iter=100) if len(param) > 0 else model
+    regr.fit(X_train, y_train)
     end_time = time.time()
     print("Time to train (seconds): " + str(end_time - start_time))
     return end_time - start_time, test(y_test, regr.predict(X_test), human)
 
 data = getFinalData()
-models = [svm.SVR(), MLPRegressor(max_iter=1000), DecisionTreeRegressor(), RandomForestRegressor()]
-names = ["SVM", "NN", "DT", "RF"]
+models = [svm.SVR(), MLPRegressor(max_iter=1000), DecisionTreeRegressor(), RandomForestRegressor(), LinearRegression()]
+names = ["SVM", "NN", "DT", "RF", "LR"]
 parameters = [{'C': scipy.stats.expon(scale=1),
                'gamma': scipy.stats.expon(scale=.1),
                'degree': scipy.stats.expon(scale=1),
                'coef0': scipy.stats.expon(scale=1),
                'tol': scipy.stats.expon(scale=.01),
                'kernel': ['rbf', 'linear'],
-               'class_weight':['balanced', None],
               },
               {'activation': ['logistic', 'relu'],
                'solver': ['sgd', 'lbfgs'],
@@ -90,38 +97,48 @@ parameters = [{'C': scipy.stats.expon(scale=1),
               {'max_depth': scipy.stats.expon(scale=100),
               },
               {'max_depth': scipy.stats.expon(scale=100),
-              }]
+              },
+              {}]
 
+ln = len(models)
 t = []
-y = [[] for i in range(4)]
-avgs = [[0, 0, 0, 0] for i in range(4)]
-n = 30
+y = [[] for i in range(ln)]
+nums = [[[] for j in range(ln)] for i in range(4)]
+n = 1
 for k in range(n):
-    for i in range(len(models)):
+    for i in range(ln):
         out = train(names[i], models[i], parameters[i], *data)
         t.append(out[0])
         for j in range(4):
             y[j].append(out[1][j])
-            avgs[j][i] += out[1][j]/n
+            nums[j][i].append(out[1][j])
 
-for row in avgs: print(row)
+for row in nums: print(row)
+
+for row in nums:
+    mean, std = "", ""
+    for arr in row:
+        mean += str(stats.tmean(arr))
+        std += str(stats.tstd(arr))
+    print("Mean " + mean)
+    print("STD " + std)
+    print(stats.f_oneway(*row))
 
 fig, axes = plt.subplots(nrows=2, ncols=2, sharex=True, sharey=False)
 fig.add_subplot(111, frameon=False)
-plt.tick_params(labelcolor='none', top='off', bottom='off', left='off', right='off')
+plt.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
 plt.grid(False)
-plt.title('Performace of four models by four metrics compared to training time')
 plt.xlabel('Time to train (seconds)')
 plt.ylabel('Model performance', labelpad=40)
 
-colors, markers = ['b', 'g', 'r', 'y'], ['.', 'o', '^', 's']
+colors, markers = ['b', 'g', 'r', 'y', 'm'], ['.', 'o', '^', 's', 'p']
 ylabels = ['Mean absolute error', 'Median absolute error', 'Explained variance score', r'$R^2$' + ' score']
 for i, ax in enumerate(axes.flat):
     for j in range(len(y[i])):
-        ax.scatter(t[j], y[i][j], c=colors[j % 4], marker=markers[j % 4], label=names[j % 4])
+        ax.scatter(t[j], y[i][j], c=colors[j % ln], marker=markers[j % ln], label=names[j % ln])
     ax.set_ylabel(ylabels[i])
     h, l = ax.get_legend_handles_labels()
-    fig.legend(h[:4], l[:4], bbox_to_anchor=(0.95, 0.75), loc=1, borderaxespad=0.)
+    fig.legend(h[:ln], l[:ln], bbox_to_anchor=(0.95, 0.75), loc=1, borderaxespad=0.)
 
 plt.tight_layout()
 plt.savefig('graph.png', dpi=800, bbox_inches='tight')
